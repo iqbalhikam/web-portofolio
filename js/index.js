6d23cf27987039dbe206d60f0e7c79085bdb38e4
iqbalhikam <idiscrus@gmail.com>
2024-01-30 16:33:42 +0700

    branch 1

--- js/index.js
+++ js/index.js
@@ -1,31 +1,4 @@
-  function openCertificateModal(title, imageSrc, description) {
-    // Dapatkan elemen modal
-    var modal = document.getElementById("certificateModal");
-    var titleElement = document.getElementById("certificateTitle");
-    var imageElement = document.getElementById("certificateImage");
-    var descriptionElement = document.getElementById("certificateDescription");
 
-    // Perbarui konten modal
-    titleElement.innerHTML = title;
-    imageElement.src = imageSrc;
-    descriptionElement.innerHTML = description;
+const modal = document.getElementById("modal")
 
-    // Tampilkan modal
-    modal.style.display = "block";
-  }
-
-  function closeModal() {
-    // Dapatkan elemen modal
-    var modal = document.getElementById("certificateModal");
-
-    // Tutup modal
-    modal.style.display = "none";
-  }
-
-  // Tutup modal jika pengguna mengklik di luar modal
-  window.onclick = function (event) {
-    var modal = document.getElementById("certificateModal");
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+modal.style.display = "none"
